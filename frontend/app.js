b55163ee6f935c977273db2761f6d89e8bf1a3b8
Andrew Kvasov <kvandrew11@gmail.com>
2017-01-27 13:41:23 +0300

    News Page: app.js little, template styling

--- frontend/app.js
+++ frontend/app.js
@@ -7,15 +7,30 @@ var CommentsForm = require('./components/CommentsForm.js');
 var stores = require('./stores/stores.js');
 var commentStore = stores.commentStore;
 var loginForm = require('./components/LoginForm.jsx');
+var StoriesPage = require('./components/StoriesPage.jsx');
+var MainContainer = require('./components/MainContainer.jsx')
 
 
-ReactDOM.render(
-  <LoginForm />,
-  $('#root').get(0)
+// bootstrap link   
+// <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+$.ajax({
+            url:"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
+           	crossorigin:"anonymous",
+           	integrity:"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+            success:function(data){
+                 $("<style></style>").appendTo("head").html(data);
+            }
+        })
+
+// Static part
+ReacDom.render(
+	<MainContainer />,
+	$('body').get(0)
 );
 
+// Dynamic part
 ReactDOM.render(
-  <Comments />,
+  <StoriesPage />,
   $('#root').get(0)
 );
 
